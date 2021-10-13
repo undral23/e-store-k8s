2720f14580b94d7cac3123a4d38f6d5ba460b3a5
Munkh-Undral E <1236emu@gmail.com>
2021-10-13 18:30:10 -0500

    txn card k*s config

--- Springboot-Microservice/transaction-card-service/src/main/java/edu/miu/sa/transaction/card/CardTransactionServiceApplication.java
+++ Springboot-Microservice/transaction-card-service/src/main/java/edu/miu/sa/transaction/card/CardTransactionServiceApplication.java
@@ -3,6 +3,7 @@ package edu.miu.sa.transaction.card;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @ComponentScan("edu.miu.sa")
 //@EntityScan("edu.miu.sa.entity")
+@EnableEurekaClient
 public class CardTransactionServiceApplication {
 
 	public static void main(String[] args) {
