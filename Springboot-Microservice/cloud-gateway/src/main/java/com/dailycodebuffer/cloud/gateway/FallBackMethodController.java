179ffb202d54744f3f0388b5b577a9f728012264
Munkh-Undral E <1236emu@gmail.com>
2021-10-12 19:47:44 -0500

    changes

--- Springboot-Microservice/cloud-gateway/src/main/java/com/dailycodebuffer/cloud/gateway/FallBackMethodController.java
+++ Springboot-Microservice/cloud-gateway/src/main/java/com/dailycodebuffer/cloud/gateway/FallBackMethodController.java
@@ -6,15 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FallBackMethodController {
 
-    @GetMapping("/userServiceFallBack")
-    public String userServiceFallBackMethod() {
-        return "User Service is taking longer than Expected." +
-                " Please try again later";
-    }
-
-    @GetMapping("/departmentServiceFallBack")
-    public String departmentServiceFallBackMethod() {
-        return "Department Service is taking longer than Expected." +
-                " Please try again later";
-    }
+	@GetMapping("/serviceFallBack")
+	public String serviceFallBackBackMethod() {
+		return "Requested Service is taking longer than Expected." + " Please try again later";
+	}
 }
